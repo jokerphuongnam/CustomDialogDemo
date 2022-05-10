930ddd0273a70f0ac4efacb7cfdce8e0228187c9
Phương Nam <nampp@mapmaker.asia>
2022-05-10 14:03:13 +0700

    initial git

--- CustomDialogDemo/ContentView.swift
+++ CustomDialogDemo/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isPresented: Bool = false
     var body: some View {
-        Text("Hello, world!")
+        Button(action: onShowDialog) {
+            VStack {
+                Text("This demo for dialog")
+                    .font(.largeTitle)
+                Text("Tap to show dialog")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(20)
+            }
+        }
+        .background(Color.white)
+        .cornerRadius(16)
+        .dialog(isPresented: $isPresented, isEnableCancel: false, onDismiss: onDismiss) {
+            VStack {
+                Text("Dialog")
+                    .font(.largeTitle)
+                    .padding()
+                
+                Text("Show dialog success!")
+                    .padding()
+            }
             .padding()
+//            .background(Color.white)
+            .cornerRadius(20)
+        }
+    }
+    
+    func onShowDialog() {
+        isPresented = true
+    }
+    
+    func onDismiss() {
+        print("Dismiss")
     }
 }
 
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+#endif
---
+++ CustomDialogDemo/ExtensionDialog.swift
@@ -0,0 +1,115 @@
+//
+//  CustomDialog.swift
+//  gannha
+//
+//  Created by gannha on 10/05/2022.
+//
+
+import SwiftUI
+
+private struct CustomDialogModifier<Body>: ViewModifier where Body: View {
+    @Binding private var isPresented: Bool
+    fileprivate typealias OnDismiss = (() -> ())
+    private let onDismiss: OnDismiss?
+    private let body: Body
+    private let isEnableCancel: Bool
+    @State private var isShow: Bool = false
+    init(
+        isPresented: Binding<Bool>,
+        isEnableCancel: Bool,
+        onDismiss: OnDismiss?,
+        @ViewBuilder body: @escaping () -> Body) {
+            self._isPresented = isPresented
+            self.isEnableCancel = isEnableCancel
+            self.onDismiss = onDismiss
+            self.body = body()
+        }
+    
+    func body(content: Content) -> some View {
+        content
+            .overlay(contentOverlay, alignment: .center)
+            .fullScreenCover(
+                isPresented: $isPresented,
+                onDismiss: onDismiss) {
+                    body
+                    .background(BackgroundDialog(isPresented: $isPresented, isEnableCancel: isEnableCancel))}
+    }
+    
+    @ViewBuilder private var contentOverlay: some View {
+        if isPresented {
+            let size = UIScreen.main.bounds
+            Color.black
+                .opacity(0.4)
+                .edgesIgnoringSafeArea(.all)
+                .frame(width: size.width, height: size.height)
+        } else {
+            EmptyView()
+        }
+    }
+}
+
+private struct BackgroundDialog: UIViewRepresentable {
+    @Binding fileprivate var isPresented: Bool
+    private let isEnableCancel: Bool
+    init(isPresented: Binding<Bool>, isEnableCancel: Bool) {
+        self._isPresented = isPresented
+        self.isEnableCancel = isEnableCancel
+    }
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView()
+        DispatchQueue.main.async {
+            guard let parentView = view.superview?.superview else {
+                return
+            }
+            
+            parentView.backgroundColor = .clear
+            view.backgroundColor = .white
+            view.layer.cornerRadius = 16
+            
+            if isEnableCancel {
+                parentView.addGestureRecognizer(context.coordinator.dismissTap)
+            }
+        }
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    fileprivate class Coordinator {
+        private let parent: BackgroundDialog
+        lazy var dismissTap = UITapGestureRecognizer(target: self, action: #selector(dismiss))
+        
+        init(_ parent: BackgroundDialog) {
+            self.parent = parent
+        }
+        
+        @objc private func dismiss(_ sender: UITapGestureRecognizer) {
+            guard let parentView = sender.view,
+                  let view = parentView.subviews.first?.subviews.first else {
+                      return
+                  }
+            let touchPoint = sender.location(in: view)
+            if !(0...view.frame.width).contains(touchPoint.x) ||
+                !(0...view.frame.height).contains(touchPoint.y) {
+                parent.isPresented = false
+            }
+        }
+    }
+}
+
+public extension View {
+    func dialog<Content>(
+        isPresented: Binding<Bool>,
+        isEnableCancel: Bool = true,
+        onDismiss: (() -> ())? = nil,
+        @ViewBuilder content: @escaping () -> Content) -> some View
+    where Content: View {
+        modifier(CustomDialogModifier(isPresented: isPresented, isEnableCancel: isEnableCancel, onDismiss: onDismiss, body: content))
+    }
+}
