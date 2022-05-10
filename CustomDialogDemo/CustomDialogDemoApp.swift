46e3d5f8763bc5ab4c0a714880e997855f0259ac
Phương Nam <nampp@mapmaker.asia>
2022-05-10 10:11:15 +0700

    Initial Commit

---
+++ CustomDialogDemo/CustomDialogDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomDialogDemoApp.swift
+//  CustomDialogDemo
+//
+//  Created by gannha on 10/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomDialogDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
